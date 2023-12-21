3d83ba16cde4186f0c7a7d40c51c6ad373c21ff0
unknown <matthew.dawsonhome@gmail.com>
2023-12-21 10:52:44 -0800

    Remove SFX_behaviours.
    I think this won't be used enought to make it worth it. If I want to have
    special behaviours it would be easier to just code them manually.

--- src/resources/SFX/SFX_behaviours/SFX_behaviour.gd
+++
@@ -1,8 +0,0 @@
-class_name SFXBehaviour extends Resource
-
-@export var sfx_data: SFXData
-
-
-# Override in subclasses. Updates data assuming all parameters have already been set/updated
-func update_sfx_data() -> void:
-	pass
